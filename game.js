2400cf91eca1dd6c33060719fcb8d822b43d56e3
Jolene Kearse <70668540+JoleneKearse@users.noreply.github.com>
2022-05-27 10:53:51 -0400

    Added die images
    Added initial die image. Edited for uniform img size. Centered die in player1/player2 blocks. Added functionality to change die image according to roll.

--- game.js
+++ game.js
@@ -9,32 +9,33 @@ const player2 = document.getElementById('player2')
 const player1Scoreboard = document.getElementById('player1Scoreboard')
 const player2Scoreboard = document.getElementById('player2Scoreboard')
 const player1Dice = document.getElementById('player1Dice')
+const img1 = document.getElementById('img1')
+
 const player2Dice = document.getElementById('player2Dice')
+const img2 = document.getElementById('img2')
 const rollBtn = document.getElementById('rollBtn')
 const resetBtn = document.getElementById('resetBtn')
 // Program Necessary Variables
 let randomNumber
-let dieImg = ''
-// const imgArr = ['images/1.png', 'images/2.png', 'images/3.png', 'images/14.png', 'images/5.png', 'images/6.png', ]
+// let dieImg = ''
+
 
 // Event Listeners
 rollBtn.addEventListener('click', function() {
     // get number from 1-6
     randomNumber = Math.floor(Math.random() * 6) + 1
-    // getDieImg(randomNumber)
+    
     if (player1Turn) {
-        // displayDie()
         player1Score += randomNumber
         player1Scoreboard.textContent = player1Score
-        player1Dice.textContent = randomNumber
+        img1.src = getDieImg(randomNumber)
         player1.classList.remove('active')
         player2.classList.add('active')
         message.textContent = 'Player Two 🏁'
     } else {
-        // displayDie()
         player2Score += randomNumber
         player2Scoreboard.textContent = player2Score
-        player2Dice.textContent = randomNumber
+        img2.src = getDieImg(randomNumber)
         player2.classList.remove('active')
         player1.classList.add('active')
         message.textContent = 'Player One 🏁'
@@ -42,21 +43,17 @@ rollBtn.addEventListener('click', function() {
 
     // check if either player is the winner & change message/buttons
     if (player1Score >= 20) {
-        message.textContent = 'Player One Won! 🥳'
-        player1.classList.remove('active')
-        player2.classList.remove('active')
+        message.textContent = 'Player One Wins! 🥳'
+        removeActiveClassesAtGameEnd()
         player1.classList.add('winner')
         player2.classList.add('loser')
-        rollBtn.style.display = 'none'
-        resetBtn.style.display = 'block'
+        changeBtns()
     } else if (player2Score >= 20) {
-        message.textContent = 'Player Two Won! 🎉'
-        player1.classList.remove('active')
-        player2.classList.remove('active')
+        message.textContent = 'Player Two Wins! 🎉'
+        removeActiveClassesAtGameEnd()
         player2.classList.add('winner')
         player1.classList.add('loser')
-        rollBtn.style.display = 'none'
-        resetBtn.style.display = 'block'
+        changeBtns()
     }
 
     // switch player turns
@@ -68,37 +65,37 @@ resetBtn.addEventListener('click', function(){
 }) 
 
 // Functions
-// function getDieImg(randomNumber) {
-//     switch(randomNumber) {
-//         case 1:
-//             dieImg = 'images/1.png'
-//             break
-//         case 2:
-//             dieImg = 'images/2.png'
-//             break
-//         case 3: 
-//             dieImg = 'images/3.png'
-//             break
-//         case 4: 
-//             dieImg = 'images/4.png'
-//             break
-//         case 5: 
-//             dieImg = 'images/5.png'
-//             break
-//         case 6: 
-//             dieImg = 'images/5.png'
-//             break
-//     }
-// }
+function getDieImg(randomNumber) {
+    let dieImg = ''
+    switch(randomNumber) {
+        case 1:
+            dieImg = 'images/1.png'
+            break
+        case 2:
+            dieImg = 'images/2.png'
+            break
+        case 3: 
+            dieImg = 'images/3.png'
+            break
+        case 4: 
+            dieImg = 'images/4.png'
+            break
+        case 5: 
+            dieImg = 'images/5.png'
+            break
+        case 6: 
+            dieImg = 'images/6.png'
+            break
+    }
+    return dieImg
+}
 
-// function displayDie(dieImg) {
-//     if (player1Turn) {
-//         let img1 = document.createElement('img')
-//         img1.src = dieImg
-//         player1Dice.appendChild(img1)
-//     } else {
-//         let img2 = document.createElement('img')
-//         img2.src = dieImg
-//         player2Dice.appendChild(img2)
-//     }
-// }
+function removeActiveClassesAtGameEnd() {
+    player1.classList.remove('active')
+    player2.classList.remove('active')
+}
+
+function changeBtns() {
+    rollBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
+}
